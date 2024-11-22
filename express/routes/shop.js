15176b813e09314522e54b5005ce0e969f16fe50
Samir <abakhri.sd@gmail.com>
2024-11-23 01:47:05 +0500

    express: using pug for views and layouts

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const path = require('path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  console.log('prods', adminData.products);
+  res.render('shop', {
+    prods: adminData.products,
+    docTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;
