8dc5d6995bbfe2c8a53c1a1b275f7a65160a3d50
Samir <abakhri.sd@gmail.com>
2024-11-22 15:17:36 +0500

    body parser, post/get middlewares

--- express/app.js
+++ express/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+  console.log('This always run',);
+  next();
+});
+
 app.use('/add-product', (req, res, next) => {
   console.log('in the middleware',);
-  res.send('<h1>Add product!</h1>')
+  res.send(`
+  <form action="/product" method="post">
+    <input type="text" name="title">
+    <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+app.post('/product', (req, res, next) => {
+  console.log('req.body',req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
