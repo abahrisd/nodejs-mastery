d2e5035287c567ef70fe4cf390c14ecdda88528b
Samir <abakhri.sd@gmail.com>
2024-11-22 15:40:32 +0500

    express views

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  console.log('in the middleware',);
-  res.send(`
-  <form action="/add-product" method="post">
-    <input type="text" name="title">
-    <button type="submit">Submit</button>
-    </form>
-  `);
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
