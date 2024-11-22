9e2d6b905e8f14eaf896854c247c4191a0d95dc7
Samir <abakhri.sd@gmail.com>
2024-11-22 15:23:37 +0500

    routing

--- express/app.js
+++ express/app.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log('This always run',);
-  next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-  console.log('in the middleware',);
-  res.send(`
-  <form action="/product" method="post">
-    <input type="text" name="title">
-    <button type="submit">Submit</button>
-    </form>
-  `);
-});
 
-app.post('/product', (req, res, next) => {
-  console.log('req.body',req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('in another middleware',);
-  res.send('<h1>Hello from express!</h1>')
-});
 
 app.listen(3000);
---
+++ express/routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  console.log('in the middleware',);
+  res.send(`
+  <form action="/product" method="post">
+    <input type="text" name="title">
+    <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+router.post('/product', (req, res, next) => {
+  console.log('req.body',req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
