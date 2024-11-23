bb2d83b72d2e02c4adc57580049162deab678a18
Samir <abakhri.sd@gmail.com>
2024-11-23 13:42:54 +0500

    express: mvc - model

--- express/controllers/products.js
+++ express/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -12,14 +12,14 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   console.log('req.body',req.body);
-  products.push({
-    title: req.body.title,
-  });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-  console.log('prods', products);
+  const products = Product.fetchAll();
+
   res.render('shop', {
     prods: products,
     hasProds: products.length > 0,
