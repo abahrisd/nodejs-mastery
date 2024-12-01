f3b5c5c10a3bf3f475fa1df3f8c888e798094417
Samir <abakhri.sd@gmail.com>
2024-12-01 15:04:06 +0500

    mongodb: mongoose schema methods

--- express/models/user.js
+++ express/models/user.js
@@ -24,7 +24,30 @@ const userSchema = new Schema({
       }
     }],
   },
-})
+});
+
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: product._id,
+        quantity: newQuantity,
+      });
+    }
+
+    this.cart = {
+      items: updatedCartItems,
+    };
+    this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 
@@ -64,29 +87,6 @@ module.exports = mongoose.model('User', userSchema);
 //   }
 //
 //   addToCart(product) {
-//     const cartProductIndex = this.cart.items.findIndex(cp => {
-//       return cp.productId.toString() === product._id.toString();
-//     });
-//     let newQuantity = 1;
-//     const updatedCartItems = [...this.cart.items];
-//
-//     if (cartProductIndex >= 0) {
-//       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-//       updatedCartItems[cartProductIndex].quantity = newQuantity;
-//     } else {
-//       updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity});
-//     }
-//
-//     const db = getDb();
-//     const updatedCart = {
-//       items: updatedCartItems,
-//     };
-//
-//     return db.collection('users')
-//       .updateOne(
-//         {_id: new ObjectId(this._id)},
-//         {$set: {cart: updatedCart}}
-//       );
 //   }
 //
 //   deleteProductFromCart(productId) {
