e947595d6434f053958c123eeff15a03d87116ae
Samir <abakhri.sd@gmail.com>
2024-12-03 16:19:43 +0500

    auth: use middleware as route guard

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -8,12 +9,12 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 //
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 //
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 //
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
